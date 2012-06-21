b711653dcbfd2d329d04cc45c4abf1d3ab50c8df
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2012-06-21 00:33:49 -0700

    Docs: skipping extension build if we're on RTD

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages, Extension
 from subprocess import Popen, PIPE
 import platform
+import os
+
+
+# Don't build extensions if we're on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 
 # Determine the name for LLVM config binary
 for path in ["llvm-config-3.1", "llvm-config"]:
-    proc = Popen(["which", path], stdout=PIPE)
+    proc = Popen(["which", path], stdout=PIPE, stderr=PIPE)
     if not proc.wait():
         LLVM_CONFIG = proc.stdout.read().strip()
         break
 else:
-    raise OSError("Could not locate a suitable llvm-config binary")
+    if not on_rtd:
+        raise OSError("Could not locate a suitable llvm-config binary")
 
 
 def llvm_config(*args):
@@ -35,32 +41,36 @@ def link_args():
     return args
 
 
-llvm_version = llvm_config("--version")[0]
+ext_modules = []
 
-# HACK this isn't really a Python extension, however it's
-# a valid shared library so we'll use the available facilities.
-llvm = Extension(
-    "nos.llvm._llvm", ["src/_llvm.cc"],
-    include_dirs=llvm_config("--includedir"),
-    extra_compile_args=llvm_config("--cxxflags"),
-    define_macros=[("NOS_LLVM_VERSION", int(llvm_version.replace(".", "")))],
-    library_dirs=llvm_config("--libdir"),
-    extra_link_args=link_args()
-)
+if not on_rtd:
+    llvm_version = llvm_config("--version")[0]
+    # HACK this isn't really a Python extension, however it's
+    # a valid shared library so we'll use the available facilities.
+    ext_modules.append(
+        Extension(
+            "nos.llvm._llvm", ["src/_llvm.cc"],
+            include_dirs=llvm_config("--includedir"),
+            extra_compile_args=llvm_config("--cxxflags"),
+            define_macros=[("NOS_LLVM_VERSION", int(llvm_version.replace(".", "")))],
+            library_dirs=llvm_config("--libdir"),
+            extra_link_args=link_args()
+        )
+    )
 
 setup(name="nos",
-      version="0.1.0",
+      version="0.0.0",
       description="Run-time LLVM-based compiler for CPython functions",
       author="Dimitri Tcaciuc",
       author_email="dtcaciuc@gmail.com",
       url="https://github.com/dtcaciuc/nos",
       license="MIT License",
-      classifiers=["Development Status :: 3 - Alpha",
+      classifiers=["Development Status :: 2 - Pre-Alpha",
                    "Intended Audience :: Developers",
                    "Natural Language :: English",
                    "Programming Language :: Python :: 2.7",
                    "Topic :: Software Development :: Libraries"],
       install_requires=["nose", "unittest2"],
-      ext_modules=[llvm],
+      ext_modules=ext_modules,
       packages=find_packages(),
      )
