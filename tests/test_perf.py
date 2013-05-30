0bc47076e61a895e5f47d5865c219854f09ba53c
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2013-05-30 00:48:40 -0700

    Simplify performance test

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -14,46 +14,40 @@ Double3 = Array(Double, (3,))
 
 X, Y, Z = range(3)
 
-@function(d=DoubleNx3, out=DoubleNArray, n=Index, m=Index)
-def sum_1(d, out, n, m):
+@function(d=DoubleNx3, out=DoubleNArray, n=Index)
+def sum_1(d, out, n):
 
-    for k in range(m):
-        for i in range(n):
-            for j in range(i + 1, n):
-                out[X] += d[i, X] * d[j, X]
-                out[Y] += d[i, Y] * d[j, Y]
-                out[Z] += d[i, Z] * d[j, Z]
+    for i in range(n):
+        for j in range(i + 1, n):
+            out[X] += d[i, X] * d[j, X]
+            out[Y] += d[i, Y] * d[j, Y]
+            out[Z] += d[i, Z] * d[j, Z]
 
-@function(d=DoubleNx3, out=DoubleN, n=Index, m=Index)
-def sum_2(d, out, n, m):
+@function(d=DoubleNx3, out=DoubleN, n=Index)
+def sum_2(d, out, n):
 
-    for k in range(m):
-        for i in range(n):
-            for j in range(i + 1, n):
-                out[X] += d[i, X] * d[j, X]
-                out[Y] += d[i, Y] * d[j, Y]
-                out[Z] += d[i, Z] * d[j, Z]
+    for i in range(n):
+        for j in range(i + 1, n):
+            out[X] += d[i, X] * d[j, X]
+            out[Y] += d[i, Y] * d[j, Y]
+            out[Z] += d[i, Z] * d[j, Z]
 
 
-@function(d=DoubleNx3, out=DoubleN, n=Index, m=Index)
-def sum_3(d, out, n, m):
+@function(d=DoubleNx3, out=DoubleN, n=Index)
+def sum_3(d, out, n):
 
     # XXX Reading from slices is about the same as arrays,
     # but writing to slice in sum_2 is about 30% slower.
     # Writing to temporary local array regains the
     # performance; figure out why.
 
-    __out = Double3()
-    __out[X] = 0.0
-    __out[Y] = 0.0
-    __out[Z] = 0.0
+    __out = Double3((0.0, 0.0, 0.0))
 
-    for k in range(m):
-        for i in range(n):
-            for j in range(i + 1, n):
-                __out[X] += d[i, X] * d[j, X]
-                __out[Y] += d[i, Y] * d[j, Y]
-                __out[Z] += d[i, Z] * d[j, Z]
+    for i in range(n):
+        for j in range(i + 1, n):
+            __out[X] += d[i, X] * d[j, X]
+            __out[Y] += d[i, Y] * d[j, Y]
+            __out[Z] += d[i, Z] * d[j, Z]
 
     for i in range(3):
         out[i] = __out[i]
@@ -62,24 +56,23 @@ def sum_3(d, out, n, m):
 class SliceDoubleLoop(unittest.TestCase):
 
     def test(self):
-        from nitrous.module import module
+        from nitrous.module import module, dump
         from time import time
 
         m = module([sum_1, sum_2, sum_3])
-        xyz = np.random.rand(10000, 3)
-        N = 10
+        xyz = np.random.rand(30000, 3)
 
         s = np.zeros(len(xyz))
         t0 = time()
-        m.sum_1(xyz, s, len(xyz), N)
-        print "sum_1, Elapsed", (time() - t0) / N
+        m.sum_1(xyz, s, len(xyz))
+        print "sum_1, Elapsed", (time() - t0)
 
         s = np.zeros(len(xyz))
         t0 = time()
-        m.sum_2(xyz, s, len(xyz), N)
-        print "sum_2, Elapsed", (time() - t0) / N
+        m.sum_2(xyz, s, len(xyz))
+        print "sum_2, Elapsed", (time() - t0)
 
         s = np.zeros(len(xyz))
         t0 = time()
-        m.sum_3(xyz, s, len(xyz), N)
-        print "sum_3, Elapsed", (time() - t0) / N
+        m.sum_3(xyz, s, len(xyz))
+        print "sum_3, Elapsed", (time() - t0)
